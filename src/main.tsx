3fa83a1fd04487ea2911a3e005ff7a0e6665e0ac
Melissa Jo Tully <150461964+MissTully@users.noreply.github.com>
2026-09-02 18:37:37 -0400

    Load the refined LBF visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./redesign.css";
 import "./art.css";
 import "./recruit.css";
 import "./company.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
